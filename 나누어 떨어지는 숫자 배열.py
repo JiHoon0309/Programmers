05ef6e8d582664634cffce08735a50084d3ad290
ssorlawlgns <ssorlawlgns@naver.com>
2021-08-24 00:54:39 +0900

    22_나누어 떨어지는 숫자 배열

---
+++ 나누어 떨어지는 숫자 배열.py
@@ -0,0 +1,9 @@
+def solution(arr, divisor):
+    result=[]
+    for i in range(len(arr)):
+        if arr[i]%divisor==0:
+            result.append(arr[i])
+    if len(result)==0:
+        result.append(-1)
+    result.sort()
+    return result
