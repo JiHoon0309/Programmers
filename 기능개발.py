dc169233d9ddb100d5efa01c2ba1fbd6166a14be
ssorlawlgns <ssorlawlgns@naver.com>
2021-09-02 21:35:47 +0900

    34_기능개발(02)

--- 기능개발.py
+++ 기능개발.py
@@ -2,18 +2,21 @@ def solution(progresses, speeds):
     answer = []
     arr=[]
     c=1
-    temp=0
     for i in range(len(progresses)):
         count=0
         while 100>progresses[i]:
             progresses[i]+=speeds[i]
             count+=1
         arr.append(count)
-    for i in arr:
-        if arr[i]>arr[i+1]:
+    temp=arr[0]
+    for i in range(1,len(arr)):
+        if temp>=arr[i]:
+            c+=1
+        else:
             temp=arr[i]
-            
-
-
-    answer.append(1)
+            answer.append(c)
+            c=1
+    answer.append(c)    
     return answer
+
+print(solution([95, 90, 99, 99, 80, 99],[1, 1, 1, 1, 1, 1]))
