1eff4b11dcbc63c39341561d0bd1fdae248c6069
ssorlawlgns <ssorlawlgns@naver.com>
2021-09-24 22:01:13 +0900

    50_체육복(02)

--- 체육복.py
+++ 체육복.py
@@ -1,28 +1,11 @@
 def solution(n, lost, reserve):
-    setlost=set(lost)
-    setreserve=set(reserve)
-    settemp1=set()
-    settemp2=set()
+    setlost=set(lost)-set(reserve)
+    setreserve=set(reserve)-set(lost) #본인 중복 제외
 
-    setlost-=setreserve #본인 체육복 잃어버려서 본인꺼 입음
+    for i in setreserve:
+        if i-1 in setlost:
+            setlost.remove(i-1)   #앞번호 없을떄 빌려주고 앞번호 제외
+        elif i+1 in setlost:
+            setlost.remove(i+1)   #뒷번호 없을때 빌려주고 뒷번호 제외
     
-    
-    for i in setlost:   
-        if i+1 in setreserve:
-            settemp1.add(i)
-            settemp2.add(i+1)
-    setlost-=settemp1
-    setreserve-=settemp2  #뒷번호 체육복 빌림
-
-    for i in setlost:    
-        if i-1 in setreserve:
-            settemp1.add(i)
-            settemp2.add(i-1)
-    setlost-=settemp1
-    setreserve-=settemp2  #앞번호 체육복 빌림
-
-        
     return n-len(setlost)
-
-
-print(solution(	5, [1,2,3,4,5], [3,5]))
