25d7bcebe81821f09d94468e50870951d8b0d638
ssorlawlgns <ssorlawlgns@naver.com>
2021-09-27 23:52:44 +0900

    52_JadenCase 문자열 만들기(02)

---
+++ JadenCase 문자열 만들기.py
@@ -0,0 +1,13 @@
+def solution(s):
+    answer=[]
+    word=s.split(' ')
+    for i in range(len(word)):
+        word[i]=word[i].lower()
+        result=list(word[i])
+        if len(result)==0:
+            continue
+        else:
+            result[0]=result[0].upper()
+        join=''.join(result)
+        answer.append(join)
+    return ' '.join(answer)
