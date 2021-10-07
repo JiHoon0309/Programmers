25881b74d2ceee5a6d88de8b709df7e7b497c2fc
ssorlawlgns <ssorlawlgns@naver.com>
2021-10-07 23:39:35 +0900

    56_이진 변환 반복하기(02)

--- 이진 변환 반복하기.py
+++ 이진 변환 반복하기.py
@@ -1,21 +1,15 @@
 def solution(s):
-    word=list(s)
-    next=[]
     count=0
-    loop=0
-    result=[]
-    while True:
-        for i in range(len(word)):
-            if word[i]=='0':
-                count+=1
-            else:
-                next.append(word[i])
-        word==next
-        b=bin(len(next))
-        word=list(b)
-        word.remove(word[:1])
-        loop+=1
-    result.append(loop,count)
-    return b
+    count1=0
+    
+    while s!='1':
+        count+=1
+        count2=len(s)
+        count1+=s.count('0')   #제거할 0의 개수
+        count2-=s.count('0')   #0 제거 후 길이
+        s=bin(count2)[2:]
 
-print(solution("110010101001"))
+    return [count, count1]        
+
+
+print(solution("01110"))
