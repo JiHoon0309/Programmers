510cfb40b4d9766d804c9e2c5ca6bd7f68d35152
ssorlawlgns <ssorlawlgns@naver.com>
2021-10-11 22:35:01 +0900

    58_구명보트(03)

--- 구명보트.py
+++ 구명보트.py
@@ -1,27 +1,18 @@
 def solution(people, limit):
     people.sort()
-    sum=0
     count=0
-    i=1
-    sum=people[0]+people[len(people)-1]
-    while True:
-        if people[0]==(limit//2) and people.count(limit//2)==1:
+    while len(people)>0:
+        sum=people[0]+people[-1]
+        if sum<=limit:
             count+=1
-            people.remove(people[0])
-
-        if people[0]>limit//2:
-            count+=len(people)
+            people.pop()
+            people.pop(0)
+        else:
+            people.pop()
+            count+=1
+        if len(people)==1:
+            count+=1
             break
-
-        if sum>limit:
-            sum=people[0]+people[len(people)-1-i]
-            i+=1
-        if sum==100:
-            people.remove(people[len(people)-i])
-            people.remove(people[0])
-            count+=1
-        if len(people)-i==1:
-            people.remove(people[0])
-
     return count
-print(solution([70, 50, 80, 30]	,100))
+
+print(solution([70, 80, 50]	,100))
