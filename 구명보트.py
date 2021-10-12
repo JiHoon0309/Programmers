b7802713b54f9596ead410a8eff0578b603a7444
ssorlawlgns <ssorlawlgns@naver.com>
2021-10-12 23:21:37 +0900

    58_구멍보트(04)

--- 구명보트.py
+++ 구명보트.py
@@ -1,18 +1,14 @@
 def solution(people, limit):
     people.sort()
     count=0
-    while len(people)>0:
-        sum=people[0]+people[-1]
-        if sum<=limit:
-            count+=1
-            people.pop()
-            people.pop(0)
-        else:
-            people.pop()
-            count+=1
-        if len(people)==1:
-            count+=1
-            break
+    i=0
+    j=len(people)-1
+    while i<=j:
+        count+=1
+        if people[i]+people[j]<=limit:
+            i+=1
+        j-=1
+
     return count
 
-print(solution([70, 80, 50]	,100))
+print(solution([20,30,40,60,80,90],100))
