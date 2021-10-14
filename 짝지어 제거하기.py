be50fa288d531a7cd039dc8b52ae11446fd93594
ssorlawlgns <ssorlawlgns@naver.com>
2021-10-14 23:50:58 +0900

    60_짝지어 제거하기(01)

---
+++ 짝지어 제거하기.py
@@ -0,0 +1,18 @@
+def solution(s):
+    word=list(s)
+    i=1
+    while True:
+        if word[i-1]==word[i]:
+            word.remove(word[i-1])
+            word.remove(word[i-1])
+            i=1
+        else:
+            i+=1
+        if i==len(word) or len(word)==0:
+            break
+
+    if i==1:
+        result=1
+    else:
+        result=0
+    return result
