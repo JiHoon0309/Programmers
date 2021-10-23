da6a678f121417bb56802dfb701b1209f54b9dcb
ssorlawlgns <ssorlawlgns@naver.com>
2021-10-23 23:45:22 +0900

    44_실패율(04)

--- 실패율.py
+++ 실패율.py
@@ -1,18 +1,14 @@
 def solution(N, stages):
-    result=[]
-    count3=0
+    dic={}
+    total=len(stages)
+
     for i in range(1,N+1):
-        count=0
-        count2=0
-        for j in range(len(stages)):
-            if stages[j]>=i:
-                count+=1
-            if stages[j]==i:
-                count2+=1
-        count3+=1
-        result.append([count2/count,count3])
-    result.sort(reverse=True, key=lambda x:x[0])
-    for i in range(len(result)):
-        result[i].pop(0)
-    answer=sum(result,[])
-    return answer
+        if total!=0:
+            now=stages.count(i)
+            rate=now/total
+            dic[i]=rate
+            total-=now
+        else:
+            dic[i]=0
+    
+    return sorted(dic, key=lambda x: dic[x], reverse=True)
