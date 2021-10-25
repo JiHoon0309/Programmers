deedeb04d20426e5916d148ce849b3d072566437
ssorlawlgns <ssorlawlgns@naver.com>
2021-10-25 23:55:00 +0900

    65_더 맵게(01)

---
+++ 더 맵게.py
@@ -0,0 +1,16 @@
+import heapq
+def solution(scoville, K):
+    heapq.heapify(scoville)
+    count=0
+    if scoville[0]>=K:
+        return -1
+    while len(scoville)>=2:
+        if scoville[0]>=K:
+            break
+        count+=1
+        scoville_min=heapq.heappop(scoville)
+        scoville_min2=heapq.heappop(scoville)
+        new=scoville_min+(scoville_min2*2)
+        heapq.heappush(scoville, new)
+
+    return count
