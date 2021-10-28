61ad0f156f0ff44837360e76bac448e316d2f111
ssorlawlgns <ssorlawlgns@naver.com>
2021-10-28 23:44:40 +0900

    66_예상 대진표(02)

--- 예상 대진표.py
+++ 예상 대진표.py
@@ -1,11 +1,19 @@
+import math
 def solution(n,a,b):
     count=1
-    while n>2:
-        if a<=n//2 and b>n//2:
-            count+=1
-            b=round(b/2)
-            a=round(a/2)
-            n=n//2
+    temp=0
+    if a>b:
+        temp=a
+        a=b
+        b=temp
+    while n>=2:
+        if a==0:
+            a+=1
+        if b%2==0 and b-a==1:
+            break
         else:
-            return count
+            count+=1
+            a=math.ceil(a/2)
+            b=math.ceil(b/2)
+
     return count
