e432e44d6169410d4fa8e738314df6c9837e7cc7
ssorlawlgns <ssorlawlgns@naver.com>
2021-10-31 23:48:19 +0900

    68_피보나치 수(02)

--- 피보나치 수.py
+++ 피보나치 수.py
@@ -1,5 +1,6 @@
 def solution(n):
-    if n==1 or n==2:
-        return 1
-    else:
-        return (solution(n-2)+solution(n-1))%1234567
+    result=[0,1]
+    for i in range(2,n+1):
+        result.append((result[i-1]+result[i-2])%1234567)
+
+    return result[-1]
