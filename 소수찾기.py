758e7e9808f8b0c6ee8f3e6933d9361f9c29bafc
ssorlawlgns <ssorlawlgns@naver.com>
2021-11-08 19:59:53 +0900

    73_소수찾기

---
+++ 소수찾기.py
@@ -0,0 +1,7 @@
+def solution(n):
+    arr=[True]*(n+1)
+    for i in range(2,int(n**0.5)+1):
+        if arr[i]==True:
+            for j in range(i+i,n+1,i):
+                arr[j]=False
+    return arr.count(True)-2
